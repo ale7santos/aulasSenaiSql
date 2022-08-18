24a00ebd67eabf1f7eda82d30bd2167250b5faf4
ale7santos <alessandro.jdsantos@gmail.com>
2022-08-18 10:56:55 -0300

    first commit

---
+++ ExercicioNovoBanco.sql
@@ -0,0 +1,17 @@
+SELECT * FROM segundobanco.aluno;
+
+insert into aluno values ("Myrian Vilela Malaquias","(84) 99969-8898","Rua Maria Edite da Costa","F",42);
+
+insert into aluno values ("Maria Isabela Inácio Teodoro","(37) 98422-4573","Rua Antônio Rezende Costa","F",67);
+
+insert into aluno values ("Maurício Robadey Coutinho","(61) 99240-3461","Quadra QN 316 Conjunto 1","M",35);
+
+insert into aluno values ("Nei Oliveira Santana","(84) 96918-1682","Rua do Sabal","M",38);
+
+insert into aluno values ("Emanoel Serravalle Fonseca","(82) 97916-0662","Quadra D","M",78);
+
+insert into aluno values ("Carlin Cormack Mourão","(21) 98922-0652","Rua Queluz","M",22);
+
+insert into aluno values("Paula Monnerat Dutra","(47) 97643-1304","Rua Efigênia Gomes Albino","F",42);
+
+insert into aluno values(null,null,null,null,null);
